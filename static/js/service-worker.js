acb169f56849f9922945e573ef9c12213716481c
root <martin.seeger2002@gmail.com>
2025-01-23 01:56:43 -0600

    changed some stuff about app mode for android

--- static/js/service-worker.js
+++ static/js/service-worker.js
@@ -1,13 +1,47 @@
-self.addEventListener('install', event => {
-  console.log('Service Worker installing.');
-  // Add caching logic here if needed
+const CACHE_NAME = 'plugz-wallet-v1';
+const ASSETS_TO_CACHE = [
+  '/wallet',
+  '/static/css/style.css',
+  '/static/js/main.js',
+  '/static/images/icon-192x192.png',
+  '/static/images/icon-512x512.png',
+  'https://unpkg.com/swiper/swiper-bundle.min.css',
+  'https://unpkg.com/swiper/swiper-bundle.min.js',
+  'https://cdn.jsdelivr.net/npm/qrcode/build/qrcode.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
 });
 
-self.addEventListener('activate', event => {
-  console.log('Service Worker activating.');
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-  console.log('Fetching:', event.request.url);
-  // Add fetch handling logic here if needed
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
 }); 
